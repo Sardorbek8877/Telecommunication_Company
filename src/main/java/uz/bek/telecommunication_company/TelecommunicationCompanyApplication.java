155832a950b2ecf5844f66850d444acceb20afde
Sardorbek8877 <sardorbeksafarov8844@gmail.com>
2023-09-01 19:11:27 +0200

    create Project

---
+++ src/main/java/uz/bek/telecommunication_company/TelecommunicationCompanyApplication.java
@@ -0,0 +1,13 @@
+package uz.bek.telecommunication_company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelecommunicationCompanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelecommunicationCompanyApplication.class, args);
+    }
+
+}
